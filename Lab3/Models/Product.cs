5709ac06a6427bac9068cce6dc821832e405e8ab
Andrews <rakdav@inbox.ru>
2024-11-23 09:50:40 +0200

    Добавьте файлы проекта.

---
+++ Lab3/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lab3.Models
+{
+    public class Product
+    {
+        public long ProductId { get; set; }
+        public required string Mame { get; set; }
+        [Column(TypeName ="decimal(8,2)")]
+        public decimal Price { get; set; }
+        public long CategoryId { get; set; }
+        public Category? Category { get; set; }
+        public long SupplierId { get; set; }
+        public Supplier? Supplier { get; set; }
+
+    }
+}
