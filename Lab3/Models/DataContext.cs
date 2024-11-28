88a90fb26e7ee1265047afd7464db8e5335b145e
Andrews <rakdav@inbox.ru>
2024-11-28 16:11:50 +0200

    Добавьте файлы проекта.

---
+++ Lab3/Models/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Lab3.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options){}
+        public DbSet<Product> Products => Set<Product>();
+        public DbSet<Category> Categories => Set<Category>();
+        public DbSet<Supplier> Suppliers => Set<Supplier>();
+    }
+}
---
+++ Lab3/Program.cs
@@ -0,0 +1,22 @@
+using Lab3.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DataContext>(opts =>
+{
+    opts.UseSqlServer(builder.Configuration[
+        "ConnectionStrings:ProductConnection"]);
+
+builder.Services.AddDbContext<DataContext>(opts => {
+    opts.UseSqlServer(builder.Configuration[
+    "ConnectionStrings:ProductConnection"]);
+    opts.EnableSensitiveDataLogging(true);
+});
+var app = builder.Build();
+app.UseMiddleware<Lab3.TestMiddleware>();
+app.MapGet("/", () => "Hello World!");
+var context = app.Services.CreateScope().ServiceProvider.
+    GetRequiredService<DataContext>();
+SeedData.SeedDatabase(context);
+app.Run();
