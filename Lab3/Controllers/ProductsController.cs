ce8c049095191f2ac289c76104a83d2a97fb42df
Andrews <rakdav@inbox.ru>
2024-11-28 16:38:02 +0200

    last 28.11

---
+++ Lab3/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using Lab3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.AspNetCore.RateLimiting;
+
+namespace Lab3.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [EnableRateLimiting("fixedWindow")]
+    public class ProductsController : ControllerBase
+    {
+        private DataContext db;
+
+        public ProductsController(DataContext _db)
+        {
+            this.db = _db;
+        }
+        [HttpGet]
+        public IAsyncEnumerable<Product> GetProducts()
+        {
+            return db.Products.AsAsyncEnumerable();
+        }
+        [HttpGet("{id}")]
+        [DisableRateLimiting]
+        public async Task<IActionResult> GetProduct(long id)
+        {
+            Product? p = await db.Products.FindAsync(id);
+            if (p == null) return NotFound();
+            return Ok(p);
+        }
+        [HttpPost]
+        public async Task<IActionResult> SaveProduct(Product )
+    }
+}
